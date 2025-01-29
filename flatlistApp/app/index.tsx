74ff05831edd82f46b7b9fd80301527bcb366a3e
Sahanar5414 <145374592+Sahanar5414@users.noreply.github.com>
2025-01-29 11:59:33 -0600

    lists

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,8 +7,30 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import { useState } from "react";
 
 export default function Index() {
+type dataType = {
+  id: string; //unique identifier, like ID number
+  title: string // what list will show
+};
+
+const DATA: dataType[] = [
+  {id: '1', title: 'First'},
+  {id: '2', title: 'Second'},
+  {id: '3', title: 'Third'},
+  {id: '4', title: 'Fourth'},
+  {id: '5', title: 'Fifth'},
+]
+
+const [selectedId, setSelectedId] = useState<string>("");
+
+//this function will accept a paremeter of type dataType
+//call parameter item
+const selectedList = (item: dataType) => {
+  console.log("selected " + item.title)
+  setSelectedId(item.id);
+}
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +38,27 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            extraData={selectedId}
+            renderItem={({item}) => (
+             <TouchableOpacity onPress={() => selectedList(item)}>
+                <View style={[styles.titleContainer, 
+                 {backgroundColor: 
+                  item.id === selectedId
+                  ? colors.primary: colors.secondary,} 
+                ]}>
+                    <Text style={[styles.titleText, {color: 
+                      item.id === selectedId
+                      ? colors.text.light
+                      : colors.text.dark
+                    }]}>{item.title}</Text>
+                </View>
+             </TouchableOpacity>
+
+            )}
+          />
         </View>
       </View>
     </View>
@@ -32,6 +74,7 @@ const styles = StyleSheet.create({
     width: 300,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    backgroundColor: 'indianred'
   },
   titleText: {
     fontSize: 24,
